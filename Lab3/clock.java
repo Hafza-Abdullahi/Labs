236bff081259346bbcdc3f353bfa1c38fb5fb269
(Student) - Hafza Abdullahi <c00286249@itcarlow.ie>
2023-10-08 18:10:42 +0100

    clock

--- Lab3/clock.java
+++ Lab3/clock.java
@@ -1,17 +1,25 @@
 package Lab3;
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class clock {
-    public static void main(String args[]) {
-        Long msElapsed;
-        Long OneSecond;
+    public static void main(String args[]) throws InterruptedException {
+        Long current;
+        Long oneMinute;
         Calendar cal = Calendar.getInstance();
         Time t = new Time(cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
     
         
-        msElapsed = System.currentTimeMillis();
-        OneSecond = System.currentTimeMillis() + 1000;
+        current = System.currentTimeMillis();
+        oneMinute = current + 60000; //60secs later 
         
+        while( System.currentTimeMillis() < oneMinute) {  //system.currrentTimeMillis() used so it updates
+            
+            t.tick();
+            System.out.println(t);
+            TimeUnit.SECONDS.sleep(1);      //wait one second, throws interruptedException
+            
+        }
         
 
         }
